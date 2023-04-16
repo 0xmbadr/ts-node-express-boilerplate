74bbb03730eb6f7ef24fc20e3ef7e2d291227622
MrCarbene <Muhammad_Fouda@proton.me>
2023-04-16 13:43:34 +0200

    add Error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,37 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { environment } from './config';
+import {
+  ApiError,
+  ErrorType,
+  InternalError,
+  NotFoundError,
+} from './core/ApiError';
+import Logger from './core/Logger';
 
 const app = express();
+
+// catch 404
+app.use((req, res, next) => next(new NotFoundError()));
+
+// Middleware Error Handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    ApiError.handle(err, res);
+    if (err.type === ErrorType.INTERNAL)
+      Logger.error(
+        `500 - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
+      );
+  } else {
+    Logger.error(
+      `500 - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
+    );
+    Logger.error(err);
+    if (environment === 'development') {
+      return res.status(500).send(err);
+    }
+    ApiError.handle(new InternalError(), res);
+  }
+});
 
 export default app;
