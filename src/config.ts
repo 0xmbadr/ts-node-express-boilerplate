b8e8943ac0d3d9a8418c23f9ddd7c167fc1b288d
MrCarbene <Muhammad_Fouda@proton.me>
2023-04-16 13:23:16 +0200

    add winston logger

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,3 @@
 export const port = process.env.PORT || 8000;
+export const environment = process.env.NODE_ENV || 'development';
+export const logDirectory = process.env.LOG_DIR;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import app from './app';
 import { port } from './config';
+import Logger from './core/Logger';
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+app
+  .listen(port, () => {
+    // console.log(`server is running`);
+    Logger.info(`server is running at port: ${port} `);
+  })
+  .on('error', (e) => Logger.error(e));
